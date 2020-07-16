8b0e7d89301b35220b2c3fab07c2d817485b40ec
NickyEXE <dovern42@gmail.com>
2020-07-16 16:59:13 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Superhero.create(name: "America Chavez", power: "Punching through dimensions", weakness: "Authority", power_level: 7, good: true)
+Superhero.create(name: "Squirrel Girl", power: "Eating Nuts and Kicking Butts", weakness: "Bad database structures", power_level: 10, good: true)
+Superhero.create(name: "Sue Storm", power: "Invisibility, energy fields", weakness: "Reed", power_level: 7, good: true)
+Superhero.create(name: "Loki", power: "Tricksiness", weakness: "Just can't friggin help it, can he", power_level: 8, good: false)
+Superhero.create(name: "Wiccan", power: "Can literally just change the universe with a thought.", weakness: "Puberty, having a boyfriend who's too good for him", power_level: 10, good: true)
+Superhero.create(name: "Hulkling", power: "Shapeshifter, not being the hulk but being mistaken for him because he's green.", weakness: "Having a boyfriend who may have accidentally wished him into existence, boy that's kind of a mind****", power_level: 4, good: true)
